7e6fdf8a4f5c50a0a57dfb78da0c68b3db7d5f8a
JiHoonAHN <ahnjh1028@naver.com>
2022-05-11 09:53:07 +0900

    Initial Commit

---
+++ gitignore/gitignoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitignoreApp.swift
+//  gitignore
+//
+//  Created by Ji-hoon Ahn on 2022/05/11.
+//
+
+import SwiftUI
+
+@main
+struct gitignoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
