b726985c66275e614f2daa24912dbf405ce17d34
JiHoonAHN <ahnjh1028@naver.com>
2022-05-11 10:16:17 +0900

    [CREATE] .gitignore

--- gitignore/ContentView.swift
+++ gitignore/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("gitignore")
+                .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        .previewLayout(.fixed(width: 568, height: 320))
     }
 }
