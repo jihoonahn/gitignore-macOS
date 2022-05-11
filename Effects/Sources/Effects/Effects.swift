54f18692c86a340d2881e5495efed92af24e0b3b
JiHoonAHN <ahnjh1028@naver.com>
2022-05-11 19:58:33 +0900

    [DELETE] Asset

--- Effects/Sources/Effects/Effects.swift
+++
@@ -1,6 +0,0 @@
-public struct Effects {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- gitignore/Sources/Utils/gitignoreColor.swift
+++
@@ -1,6 +0,0 @@
-import SwiftUI
-
-public struct ColorManager {
-    static let backgroundColor = Color("PageBackgroundColor")
-    static let navigationItemColor = Color("NavigationBarColor")
-}
