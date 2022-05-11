99d3fac79ad61251bcb860de2eccabbf2383a66f
JiHoonAHN <ahnjh1028@naver.com>
2022-05-11 12:10:21 +0900

    [UPDATE] 파일 경로 변경

--- gitignore/Sources/App/gitignoreApp.swift
+++ gitignore/Sources/App/gitignoreApp.swift
@@ -6,12 +6,13 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 @main
 struct gitignoreApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.windowStyle(HiddenTitleBarWindowStyle())
     }
 }
