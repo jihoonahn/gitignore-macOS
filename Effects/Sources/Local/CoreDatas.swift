ce6390944ad230a0622c73d9cd0741d6006b1efe
JiHoonAHN <ahnjh1028@naver.com>
2022-05-26 23:29:43 +0900

    [CREATE] CoreData

--- Effects/Sources/Local/CoreDatas.swift
+++ Effects/Sources/Local/CoreDatas.swift
@@ -1,29 +1,41 @@
 import Foundation
 import Combine
 import CombineCoreData
+import CoreData
 
 public protocol CoreDatas{
-    func addList()
-    func fetchPersons()
-    func deleteAllPersons()
+    func addList(title : String, tag : [String], gitignoreString : String) -> CoreDataSaveModelPublisher
+    func fetchPersons() -> CoreDataFetchResultsPublisher<List>
+    func deleteAllPersons(title : String) -> CoreDataDeleteModelPublisher
 }
 
 public final class CoreDataService : BaseCoreDataService, CoreDatas{
-    private var bag : [AnyCancellable] = .init()
-    
+    let coreDataStore: CoreDataStoring! = nil
 }
 
 
 public extension CoreDataService{
-    func addList()  {
+    func addList(title : String, tag : [String], gitignoreString : String) -> CoreDataSaveModelPublisher  {
         let action : Action = {
-            
+            let list : List = self.coreDataStore.createEntity()
+            list.title =  title
+            list.date = Date()
+            list.tag = tag
+            list.gitignoreString = gitignoreString
         }
+        return coreDataStore
+            .publisher(save: action)
+        
     }
-    func fetchPersons()  {
-
+    func fetchPersons() -> CoreDataFetchResultsPublisher<List> {
+        let request  = NSFetchRequest<List>(entityName: List.entityName)
+        return coreDataStore
+            .publisher(fetch: request)
     }
-    func deleteAllPersons() {
-
+    func deleteAllPersons(title : String)  -> CoreDataDeleteModelPublisher{
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: List.entityName)
+        request.predicate = NSPredicate(format: "title LIKE[cd] %@", title)
+        return coreDataStore
+            .publisher(delete: request)
     }
 }
