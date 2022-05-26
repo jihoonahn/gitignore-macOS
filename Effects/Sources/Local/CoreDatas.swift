a82ff9940e7feeb64b9d56a389a397b9f4943064
JiHoonAHN <ahnjh1028@naver.com>
2022-05-26 20:48:52 +0900

    [CREATE] List

--- Effects/Sources/Local/CoreDatas.swift
+++ Effects/Sources/Local/CoreDatas.swift
@@ -3,22 +3,22 @@ import Combine
 import CombineCoreData
 
 public protocol CoreDatas{
-    func addList()
-    func fetchPersons()
-    func deleteAllPersons()
+    func addList() -> CoreDataSaveModelPublisher
+    func fetchPersons() -> CoreDataSaveModelPublisher
+    func deleteAllPersons() -> CoreDataSaveModelPublisher
 }
 
 public final class CoreDataService : BaseCoreDataService, CoreDatas{}
 
 
 public extension CoreDataService{
-    func addList() {
+    func addList() -> CoreDataSaveModelPublisher {
         
     }
-    func fetchPersons() {
+    func fetchPersons() -> CoreDataSaveModelPublisher {
         
     }
-    func deleteAllPersons() {
+    func deleteAllPersons() -> CoreDataSaveModelPublisher{
         
     }
 }
