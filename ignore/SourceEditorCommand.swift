6a29c5f7e7bd87269c692a0cda371092a9411a6e
JiHoonAHN <ahnjh1028@naver.com>
2022-05-30 13:29:01 +0900

    [CREATE] ignore

---
+++ ignore/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  ignore
+//
+//  Created by Ji-hoon Ahn on 2022/05/30.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
